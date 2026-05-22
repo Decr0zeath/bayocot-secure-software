f1ea060f22a3201fa04aa69ebc959e9aa33ee733
Decr0zeath <decr0zeath56@gmail.com>
2026-05-23 00:27:35 +0800

    Add initial UI layout for sender, receiver, and shared process panels

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,33 @@ namespace bayocot_secure_software
         public Form1()
         {
             InitializeComponent();
+            this.btnComputeKeys.Click += BtnComputeKeys_Click;
+            this.btnEncryptSend.Click += BtnEncryptSend_Click;
+            this.btnReset.Click += BtnReset_Click;
+        }
+
+        private void BtnComputeKeys_Click(object sender, EventArgs e)
+        {
+            // TODO: Diffie-Hellman (Commit 2)
+        }
+
+        private void BtnEncryptSend_Click(object sender, EventArgs e)
+        {
+            // TODO: chunk, pad, encrypt, decrypt (Commits 4-6)
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            txtPrivA.Clear();
+            txtPubA.Clear();
+            txtPrivB.Clear();
+            txtPubB.Clear();
+            txtMessage.Clear();
+            txtSharedKey.Clear();
+            txtAesKey.Clear();
+            txtSubMessages.Clear();
+            txtEncrypted.Clear();
+            txtDecrypted.Clear();
         }
     }
 }
